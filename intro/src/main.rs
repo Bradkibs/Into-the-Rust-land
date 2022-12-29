52b6cb09a76ad866c25842d32e61ba8d947be0db
Bradleykibs <Bradleykibs@gmail.com>
2022-12-29 15:35:28 +0300

    printing hello world in 3 languages

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn greet_world(){
+    let english = "Hello World";
+    let japanese = "ハロー・ワールド";
+    let swahili = "Habari yenu walimwengu";
+    let languages = [english, japanese, swahili];
+    for language in languages.iter(){
+        println!("{}", &language);
+    }
+}
+fn main(){
+    greet_world();
 }
